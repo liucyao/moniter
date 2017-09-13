4fe8367b18af0260cc30fdf10a1c0c130e386f1d
hel2o <601902498@qq.com>
2017-09-13 14:50:39 +0800

    更新数据为firesystem

--- lib/cacti_lib.php
+++ lib/cacti_lib.php
@@ -1,27 +1,27 @@
 <?php
 function rrd($file, $datakey = 0, $num_digits = 2, $step = '360', $start = '-6h', $end = 'start+5h+54m', $xtype = 'H:i')
-    {
-        $rrd = rrd_fetch($file, array('AVERAGE', '--resolution', $step, '--start', $start, '--end', $end));
-        $filekey = array_keys($rrd['data'])[$datakey];
-        $data = $rrd['data'][$filekey];
-        foreach ($data as $k => $v) {
-            $xAxis[] = date($xtype, $k);
-            if (is_nan($v)||$v == 0) {
-                //当接受到的数据为NAN时就要加这个判断 否则 json_encode时会出错
-                  $yAxis[] = '-';
-            } else {
-                $yAxis[] = round($v, $num_digits);
-            }
-        }
-        $result['xAxis'] = $xAxis;
-        $result['yAxis'] = $yAxis;
-
-        return $result;
-    }
-function Get_rrd_path($hostname,$name_cache)
 {
-  require_once 'connect.php';
-  $sql_where = "SELECT `dtd`.data_source_path,`dt`.unit
+    $rrd = rrd_fetch($file, array('AVERAGE', '--resolution', $step, '--start', $start, '--end', $end));
+    $filekey = array_keys($rrd['data'])[$datakey];
+    $data = $rrd['data'][$filekey];
+    foreach ($data as $k => $v) {
+        $xAxis[] = date($xtype, $k);
+        if (is_nan($v)||$v == 0) {
+            //当接受到的数据为NAN时就要加这个判断 否则 json_encode时会出错
+            $yAxis[] = '-';
+        } else {
+            $yAxis[] = round($v, $num_digits);
+        }
+    }
+    $result['xAxis'] = $xAxis;
+    $result['yAxis'] = $yAxis;
+
+    return $result;
+}
+function Get_rrd_path($hostname, $name_cache)
+{
+    require_once 'connect.php';
+    $sql_where = "SELECT `dtd`.data_source_path,`dt`.unit
                 FROM data_local as dl,host,data_template as dt,data_template_data as dtd
                 WHERE `dl`.host_id=`host`.id
                 AND `dt`.id=`dl`.data_template_id
@@ -29,25 +29,28 @@ function Get_rrd_path($hostname,$name_cache)
                 AND `host`.hostname = '$hostname'
                 AND `dtd`.name_cache LIKE '%$name_cache'
                 LIMIT 1";
-  $query = mysql_query($sql_where);
-  while ($rows = mysql_fetch_array($query)) {
-    $rrd_path = $rows['data_source_path'];
-  }
-  return str_replace('<path_rra>','/mnt/rra',$rrd_path);
+    $query = mysql_query($sql_where);
+    while ($rows = mysql_fetch_array($query)) {
+        $rrd_path = $rows['data_source_path'];
+    }
+    return str_replace('<path_rra>', '/mnt/rra', $rrd_path);
 }
 
-function HostDown(){
-	$time = date("H:i:s");
-	$results = "";
-    $hostdowns = mysql_fetch_array(mysql_query('SELECT COUNT(*) FROM host WHERE status = 1 AND disabled="" '));
-    $downlists= mysql_query('SELECT * FROM host WHERE status = 1 AND disabled="" ');
-        while ($row = mysql_fetch_array($downlists)) {
-            $results .= " " . $row['description'] . " [" . $row['hostname'] . "] ";
-        }
-    if (!$hostdowns[0]) {
-      $results = "满分";
+function HostDown()
+{
+    $time = date('Y-m-d H:i:s');
+    $results = '';
+    $downs = 0;
+    $pdo = new PDO("mysql:host=192.168.99.22;dbname=swdashboard", "root", "viproot");
+    foreach ($pdo->query('SELECT ipAddress,hostName,downTime FROM swdb WHERE disabled = "NO" AND status = "DOWN"') as $row) {
+        $results .= $row["hostName"]." [".$row["ipAddress"]."]  ";
+        $downs++;
     }
-    $msg .= $results." ".$time;
-    $msgs = array('msg' =>$msg ,'time' =>$time,'downs' =>$hostdowns[0] );
+
+    if (!$downs) {
+        $results = "满分";
+    }
+    $msg .= $results."  ".$time;
+    $msgs = array('msg' =>$msg ,'time' =>$time,'downs' =>$downs );
     return $msgs;
 }
--- lib/open-falcon_lib.php
+++ lib/open-falcon_lib.php
@@ -7,59 +7,59 @@ define('ADDR_API', 'http://192.168.99.26:8080');
 //v0.2版本直接使用api获取数据
 function get_data_history_v2($endpoint, $counter, $start, $end)
 {
-  $url = ADDR_API.'/api/v1/graph/history';
-  $data = array("step" => 30,
+    $url = ADDR_API.'/api/v1/graph/history';
+    $data = array("step" => 30,
                 "start_time"  => $start,
                 "hostnames"   => [$endpoint],
                 "end_time"    => $end,
                 "counters"    => [$counter],
                 "consol_fun"  => "AVERAGE"
             );
-  $data_string = json_encode($data);
-  $headers = array();
-  $headers[] = 'Apitoken:name=rh.wang;sig=';
-  $headers[] = 'Content-Type:application/json';
-  $headers[] = 'X-Forwarded-For:127.0.0.1';
-  $curl2 = curl_init();
-  curl_setopt($curl2, CURLOPT_URL, $url);
-  curl_setopt($curl2, CURLOPT_POST, TRUE);
-  curl_setopt($curl2, CURLOPT_POSTFIELDS,$data_string);
-  curl_setopt($curl2, CURLOPT_HEADER, false);
-  curl_setopt($curl2, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
-  $content = curl_exec($curl2);
+    $data_string = json_encode($data);
+    $headers = array();
+    $headers[] = 'Apitoken:name=rh.wang;sig=';
+    $headers[] = 'Content-Type:application/json';
+    $headers[] = 'X-Forwarded-For:127.0.0.1';
+    $curl2 = curl_init();
+    curl_setopt($curl2, CURLOPT_URL, $url);
+    curl_setopt($curl2, CURLOPT_POST, true);
+    curl_setopt($curl2, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($curl2, CURLOPT_HEADER, false);
+    curl_setopt($curl2, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($curl2);
 
-  return json_decode($content, true);
+    return json_decode($content, true);
 }
 
 function get_data_last_v2($endpoint, $counter)
 {
-  $data_range = get_data_history_v2($endpoint,$counter,time()-150,time());
-  $values = array_reverse($data_range[0]['Values']);
-  for ($i=0; $i < count($values) - 1; $i++) {
-    if ($values[$i]['value'] != NULL) {
-      $last = $values[$i]['value'];
-      break;
+    $data_range = get_data_history_v2($endpoint, $counter, time()-150, time());
+    $values = array_reverse($data_range[0]['Values']);
+    for ($i=0; $i < count($values) - 1; $i++) {
+        if ($values[$i]['value'] != null) {
+            $last = $values[$i]['value'];
+            break;
+        }
     }
-  }
-  return $last;
+    return $last;
 }
 
 //v0.2版本中读取到数据后拼装成echarts的数据结构
 function get_data_history_echarts($endpoints, $counter, $start, $end)
 {
-  $x = array();
-  $y = array();
-  $datas = get_data_history_v2($endpoints, $counter, $start, $end);
-  foreach ($datas[0]['Values'] as $k => $value) {
-      if ($value[1] === null) {
-          $value[1] = '-';
-      }
-      $y[] = $value['value'];
-      $x[] = date('H:i', $value['timestamp']);
-  }
+    $x = array();
+    $y = array();
+    $datas = get_data_history_v2($endpoints, $counter, $start, $end);
+    foreach ($datas[0]['Values'] as $k => $value) {
+        if ($value[1] === null) {
+            $value[1] = '-';
+        }
+        $y[] = $value['value'];
+        $x[] = date('H:i', $value['timestamp']);
+    }
 
-  return array('x' => $x, 'y' => $y );
+    return array('x' => $x, 'y' => $y );
 }
 
 
@@ -68,19 +68,19 @@ function get_data_history_echarts($endpoints, $counter, $start, $end)
 //在v0.2版本中获取dashboard的数据时要验证,所以先登录
 function login()
 {
-  $curl = curl_init();
-  $cookie_jar = tempnam(sys_get_temp_dir(),'cookie');
-  curl_setopt($curl, CURLOPT_URL, ADDR_DASHBOARD.'/auth/login');
-  curl_setopt($curl, CURLOPT_POST, 1);
-  $request = 'name='.NAME.'&password='.PASSWORD;
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
-  curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_jar);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_HEADER, false);
-  curl_setopt($curl, CURLOPT_NOBODY, false);
-  curl_exec($curl);
-  curl_close($curl);
-  return $cookie_jar;
+    $curl = curl_init();
+    $cookie_jar = tempnam(sys_get_temp_dir(), 'cookie');
+    curl_setopt($curl, CURLOPT_URL, ADDR_DASHBOARD.'/auth/login');
+    curl_setopt($curl, CURLOPT_POST, 1);
+    $request = 'name='.NAME.'&password='.PASSWORD;
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_jar);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_NOBODY, false);
+    curl_exec($curl);
+    curl_close($curl);
+    return $cookie_jar;
 }
 
 
@@ -123,14 +123,13 @@ function get_chart_data($counter_id, $start, $end)
 //v0.1版本中获取最后一次数据
 function get_data_last($endpoints, $counter)
 {
-
     $data = array('endpoint' => $endpoints , 'counter' => $counter);
     $post_data = "[".json_encode($data)."]";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "http://192.168.99.27:9966/api/v1/graph/last");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
       "Content-Type: application/json"
